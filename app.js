93d1d2bf0fbdfa7fb7aba9840347345825c88622
benni maxon <benni.maxon@gmail.com>
2022-11-07 17:25:05 -0800

    got DOM El and set state variables for slice one. created goblin display function.

--- app.js
+++ app.js
@@ -1,11 +1,22 @@
 /* Imports */
 
 /* Get DOM Elements */
+const goblinListEl = document.querySelector('.goblins');
 
 /* State */
+let goblins = [
+    { id: 1, name: 'Bingo', hp: 2 },
+    { id: 2, name: 'Bongo', hp: 3 },
+];
+let currentId = 3;
 
 /* Events */
 
 /* Display Functions */
+function displayGoblins() {
+    goblinListEl.textContent = '';
+
+    for (let goblin of goblins)
+}
 
 // (don't forget to call any display functions you want to run on page load!)
